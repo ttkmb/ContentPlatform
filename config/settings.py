4a5e7041e450d5aeed3e1ea0328bda4dc9a1e6b9
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2024-03-25 15:02:51 +0300

    add static, main  app

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'phonenumber_field'
+    'phonenumber_field',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
